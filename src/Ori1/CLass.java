a27110ad14992c8ba9aff60bae9fee3be4e7497a
danienrique <danienriquealonso@gmail.com>
2025-10-18 16:03:10 +0200

    dsx

--- src/Ori1/CLass.java
+++ src/Ori1/CLass.java
@@ -3,7 +3,7 @@ package Ori1;
 public class CLass {
 	public static void main(String[] args) {
 		for(int i = 0;i<100;i++) {
-			System.out.println("Hola Orion");
+			System.out.println("Hola Orion pspsps");
 			
 		}
 	}
